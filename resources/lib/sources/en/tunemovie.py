8dfe4accb4a2a2a8a02b5249935046d6b10b8b62
lastship <lastship@users.noreply.github.com>
2017-07-17 11:58:06 +0200

    Update tunemovie.py

--- resources/lib/sources/en/tunemovie.py
+++ resources/lib/sources/en/tunemovie.py
@@ -30,8 +30,8 @@ class source:
     def __init__(self):
         self.priority = 1
         self.language = ['en']
-        self.domains = ['tunemovies.to', 'tunemovie.tv']
-        self.base_link = 'https://tunemovies.to'
+        self.domains = ['tunemovies.com', 'tunemovie.tv']
+        self.base_link = 'https://tunemovies.com'
         self.search_link = '/search/%s.html'
 
 
