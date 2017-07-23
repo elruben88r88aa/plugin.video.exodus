cece40048a3632487d8a47c380b23ae81d54058d
Kriss <Kr1ss-X@yandex.com>
2017-07-23 11:18:35 +0200

    Update moderator function
    To change some of the container plugin permissions

--- resources/lib/modules/control.py
+++ resources/lib/modules/control.py
@@ -212,7 +212,7 @@ def selectDialog(list, heading=addonInfo('name')):
 
 
 def moderator():
-    netloc = [urlparse.urlparse(sys.argv[0]).netloc, '', 'plugin.video.live.streamspro', 'plugin.video.phstreams', 'plugin.video.cpstreams', 'plugin.video.streamarmy', 'plugin.video.tinklepad', 'plugin.video.metallic', 'plugin.program.super.favourites']
+    netloc = [urlparse.urlparse(sys.argv[0]).netloc, '', 'plugin.video.live.streamspro', 'plugin.video.phstreams', 'plugin.video.cpstreams', 'plugin.video.tinklepad', 'script.tvguide.fullscreen', 'script.tvguide.assassins']
 
     if not infoLabel('Container.PluginName') in netloc: sys.exit()
 
